5fde2c22202a99cb953483929bd9b8cd29a4c999
Milano <jenky.w0w@gmail.com>
2015-09-25 10:09:25 +0700

    Space to indent

--- src/Facades/Api.php
+++ src/Facades/Api.php
@@ -6,12 +6,12 @@ use Illuminate\Support\Facades\Facade;
 
 class Api extends Facade 
 {
-	/**
-	 * Get the registered name of the component.
-	 *
-	 * @return string
-	 */
-	protected static function getFacadeAccessor()
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
     { 
         return 'apihelper'; 
     }
